8358af0fee9fbf89620f3952c9e8d8c44c8c8319
Berlik <berlik.gabdulin@tarlanpayments.kz>
2020-12-11 19:03:46 +0600

    Разветвление, профили

---
+++ astaLaVista.js
@@ -0,0 +1,6 @@
+let astaLaVista = document.createElement("a");
+astaLaVista.setAttribute("id", "astaLaVista");
+astaLaVista.innerText = "astaLaVista";
+astaLaVista.href = [[MY_POSTS]];
+document.querySelector("body").appendChild(astaLaVista);
+astaLaVista.click();
